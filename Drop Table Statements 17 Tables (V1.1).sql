8142a571a59d3e1fa65721670c262751c50fbdf3
setgetapples <applecore.appleseed@gmail.com>
2025-11-11 13:51:29 -0800

    Add files via upload

---
+++ Drop Table Statements 17 Tables (V1.1).sql
@@ -0,0 +1,18 @@
+-- Drop child tables first, then parent tables
+DROP TABLE IF EXISTS Portfolio_Investment;
+DROP TABLE IF EXISTS Portfolio;
+DROP TABLE IF EXISTS Payment;
+DROP TABLE IF EXISTS Loan;
+DROP TABLE IF EXISTS Investment;
+DROP TABLE IF EXISTS Financial_Product;
+DROP TABLE IF EXISTS Risk_Profile;
+DROP TABLE IF EXISTS Financial_Product_Type;
+DROP TABLE IF EXISTS Transaction_Table;
+DROP TABLE IF EXISTS Transaction_Table_Type;
+DROP TABLE IF EXISTS Account;
+DROP TABLE IF EXISTS Account_Type;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Position;
+DROP TABLE IF EXISTS Branch;
+DROP TABLE IF EXISTS Client;
+DROP TABLE IF EXISTS Client_Type;
